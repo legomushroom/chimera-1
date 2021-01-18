2c7f066b026570745e998fefd90c2a7dde7eba6b
Jarod Reid <jarod@solidalchemy.com>
2021-01-18 14:33:30 +0000

    initial

---
+++ lib/chimera.rb
@@ -0,0 +1,6 @@
+require "chimera/version"
+require "chimera/engine"
+
+module Chimera
+  # Your code goes here...
+end
---
+++ test/chimera_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ChimeraTest < ActiveSupport::TestCase
+  test "it has a version number" do
+    assert Chimera::VERSION
+  end
+end
