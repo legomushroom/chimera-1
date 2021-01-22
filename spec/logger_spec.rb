0bd4db08363d086f3763f560ddb8c2721b739aa6
Jarod Reid <jarod@solidalchemy.com>
2021-01-22 17:26:21 +0000

    create rails spec

---
+++ spec/logger_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Chimera::Logging do
+  subject do
+    Class.new do
+      include Chimera::Logging
+    end.new
+  end
+
+  describe "#logger" do
+    it "returns the rails logger" do
+      expect(subject.logger).to eq(Rails.logger)
+    end
+  end
+end
