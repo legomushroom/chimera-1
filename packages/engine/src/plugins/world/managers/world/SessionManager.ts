c24e1d4dd5d2d92c584c5b9946ba52ae31e90f0b
Jarod Reid <jarod@solidalchemy.com>
2021-01-31 05:53:48 +0000

    can find in mongoose

--- packages/engine/src/plugins/world/managers/world/SessionManager.ts
+++ packages/engine/src/plugins/world/managers/world/SessionManager.ts
@@ -1,4 +1,5 @@
 import { Service as MoleculerService } from "moleculer";
+import { Model, Schema } from "mongoose";
 
 import Engine from "../../../../Engine";
 import Manager from "../../../../Manager"
@@ -14,18 +15,22 @@ export default class SessionManager extends Manager {
   name = "world.session-manager"
   mixins = [ new Mongoose() ]
   readonly sessions: ISessionList = {}
+  readonly modelSchema = new Schema({
+    uuid: { type: String, index: true, unique: true }
+  })
+  readonly modelName = "Session"
 
   @event("connections.started")
   createSession(id: string) {
-    this.logger.degbug("received conenction event, checking to see if session was already registered")
-    this.actions.find*
-
-    this.logger.debug(`new connection received with id '${id}', waiting for ready`)
+    this.logger.debug("received conenction event, checking to see if session was already registered")
+    this.actions.find({uuid: id})
+    .then((model: Model<any>) => console.log(model))
+    // this.logger.debug(`new connection received with id '${id}', waiting for ready`)
     const connecitonName = `server.connections.${id}`
-    this.broker.waitForServices(connecitonName)
-      .then(() => {
-        this.sessions[id] = Engine.createService(new Session(id))
-      })
+    // this.broker.waitForServices(connecitonName)
+      // .then(() => {
+        // this.sessions[id] = Engine.createService(new Session(id))
+      // })
   }
 
   created() {
--- packages/engine/src/plugins/world/service_mixins/Mongoose.ts
+++ packages/engine/src/plugins/world/service_mixins/Mongoose.ts
@@ -1,23 +1,38 @@
-import mongoose from "mongoose"
+import mongoose, { FilterQuery } from "mongoose"
 import Promise from "bluebird"
 
 import Engine from "../../../Engine";
-
 import { action, Service } from "../../../Service";
+import { Context } from "moleculer";
 
 
 export default class Mongoose extends Service {
+  model?: mongoose.Model<any>
+  modelSchema?: mongoose.Schema
+  modelName?: string
+
   created() {
     this.connect()
   }
 
+  started(): Promise<void> {
+    this.model = mongoose.model(<string>this.modelName, this.modelSchema)
+    return Promise.resolve();
+  }
+
   connect() {
-    this.logger.debug("connecting to mongo")
-    mongoose.connect(Engine.config.get("world.mongo.url") || "mongodb://127.0.0.1/chimera")
+    if (mongoose.connection.readyState === 0) {
+      this.logger.debug("connecting to mongo")
+      mongoose.connect(Engine.config.get("world.mongo.url") || "mongodb://127.0.0.1/chimera")
+    }
   }
 
   @action("find")
-  find(): Promise<void> {
-    return Promise.resolve();
+  find(ctx: Context): Promise<mongoose.Model<any>[]> {
+    return new Promise((resolve) => {
+      this.model?.find(<any>ctx.params, (records) => {
+        resolve(records)
+      })
+    })
   }
 }
